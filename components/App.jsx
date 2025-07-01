8f9b07a338590dac21e35b0a1a88669237db813b
nguyenp25 <16073547+nguyenp25@users.noreply.github.com>
2025-07-01 11:19:22 -0400

    Added Header + Intro Components
    - Finished Header with redirectable buttons
    - Added Intro component for landing page
    - Also added some image assets for webpage styling

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Header from './header'
+import Intro from './intro'
 
 function App() {
   return (
     <>
       <Header />
+      <Intro />
     </>
   )
 }
---
+++ components/intro.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Intro(){
+    return (
+        <>
+        <div className="intro">
+            <div className="leftIntro">
+                <img src="../images/placeholder.jpg" alt="Profile Picture"/>
+            </div>
+            <div className="rightIntro">
+                <div>
+                    <div>HI IM PETER NGUYEN</div>
+                    <div>desc</div>
+                </div>
+            </div>  
+        </div>
+        <img src="../images/wave1.svg" />
+        </>
+    )
+}
